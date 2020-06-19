92a2f03bab979607d777cf97618b30ced86c4c4f
galalkoro <koro-galal@outlook.com>
2020-06-20 00:26:51 +0200

    intigration has been solve it

--- scripts/2-honesty-test.js
+++ scripts/2-honesty-test.js
@@ -3,33 +3,51 @@
 console.log('--- loading: honesty test');
 
 function honestyTestHandler() {
-  'use strict';
-  debugger;
+	'use strict';
+	debugger;
 
-  // read inputs from user
-  const userString = _(`Please enter something:`);
+	// read inputs from user
+	const userString = prompt(`Please enter something:`);
 
-  const confirmation = _(`Did you enter something? \nyes: "ok" \nno: "cancel"`);
+	const confirmation = confirm(`Did you enter something? \nyes: "ok" \nno: "cancel"`);
 
-  // before moving on ... type check!
-  if (typeof userString !== 'string') { throw new TypeError(); }
-  if (typeof confirmation !== 'boolean') { throw new TypeError(); }
+	// before moving on ... type check!
+	if (typeof userString !== 'string') {
+		try {
+			throw new TypeError('type error');
+		} catch (e) {
+			console.log(e.message);
+		}
+	}
 
-  // perform core logic
-  
-  // true if the user input text, false if it is empty or null
-  const didEnterSomething = userString ? _ : _; 
-  const isALiar = confirmation === didEnterSomething;
-  // display a different message depending on whether or not the user is a liar
-  const message = `You are a ${_ ? 'liar' : 'mensch'}!`;
+	if (typeof confirmation !== 'boolean') {
+		throw new TypeError();
+	}
 
-  // alert result for the user
-  alert(_);
+	const didEnterSomething = Boolean(userString) ? true : false;
+	// perform core logic
+	let isALiar;
+	if (didEnterSomething && confirmation) {
+		isALiar = 'honest';
+	} else if (didEnterSomething && confirmation === false) {
+		isALiar = 'liar';
+	} else if (didEnterSomething === false && confirmation) {
+		isALiar = 'liar';
+	} else if (didEnterSomething === false && confirmation === false) {
+		isALiar = 'honest';
+	}
 
-  // log action for the developer
-  console.log('\n--- honesty test ---');
-  console.log('userString:', typeof userString, '\n', userString);
-  console.log('confirmation:', typeof confirmation, '\n', confirmation);
-  console.log('message:', typeof message, '\n', message);
+	// true if the user input text, false if it is empty or null
 
+	// display a different message depending on whether or not the user is a liar
+	const message = `You are a ${isALiar}!`;
+
+	// alert result for the user
+	alert(message);
+
+	// log action for the developer
+	console.log('\n--- honesty test ---');
+	console.log('userString:', typeof userString, '\n', userString);
+	console.log('confirmation:', typeof confirmation, '\n', confirmation);
+	console.log('message:', typeof message, '\n', message);
 }
